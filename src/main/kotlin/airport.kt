abbbdb3abaa5f7c8b42053e7fb4880520f387b65
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-08 13:17:14 +0000

    passing test for landing a plane at an airport

--- src/main/kotlin/airport.kt
+++ src/main/kotlin/airport.kt
@@ -1,5 +1,16 @@
 class Airport() {
+    var planes: MutableList<String>
+
+    init {
+       this.planes =  ArrayList()
+    }
+
     fun hello(): String {
         return "i am an airport"
     }
+
+    fun land(plane: String) {
+        planes.add(plane)
+    }
+
 }
--- src/test/kotlin/airportTest.kt
+++ src/test/kotlin/airportTest.kt
@@ -1,14 +1,22 @@
 import junit.framework.Assert.assertEquals
-//import org.hamcrest.CoreMatchers.equalTo
-//import org.hamcrest.CoreMatchers.not
-//import org.hamcrest.MatcherAssert.assertThat
-
+import junit.framework.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 
 public class airportTest {
+//    @Before
+//    val a = Airport()
+
     @Test
     fun ` should say it is an airport `() {
         val a = Airport()
         assertEquals("i am an airport", a.hello())
+    }
+
+    @Test
+    fun ` can receive a landing plane `() {
+        val a = Airport()
+        a.land("plane1")
+        assertTrue(a.planes.contains("plane1"))
     }
 }
